b657120bba419000ca6e87e0d3a31ed66e546c9d
ZILONG LI <11096690+technolingo@users.noreply.github.com>
2018-11-29 02:29:34 +0900

    add beat for periodic tasks

--- dj_async/celery.py
+++ dj_async/celery.py
@@ -17,6 +17,25 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
 
+from celery.schedules import crontab
+app.conf.beat_schedule = {
+    'add-every-minute-contrab': {
+        'task': 'multiply_two_numbers',
+        'schedule': crontab(),
+        'args': (16, 16),
+    },
+    'add-every-5-seconds': {
+        'task': 'multiply_two_numbers',
+        'schedule': 5.0,
+        'args': (16, 16)
+    },
+    'add-every-30-seconds': {
+        'task': 'tasks.add',
+        'schedule': 30.0,
+        'args': (16, 16)
+    },
+}
+
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
